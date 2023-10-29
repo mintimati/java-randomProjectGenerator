ec269b1b05e446178c3c05034c3253a39c19dcd9
mintimati <mintimati@gmail.com>
2023-10-29 18:55:13 +0200

    program complete. for now

--- src/main/Main.java
+++ src/main/Main.java
@@ -6,6 +6,6 @@ import layers.Melody;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(new Melody().getResult());
+        System.out.println(new RandProject().getProjectDetails());
     }
 }
--- src/main/RandProject.java
+++ src/main/RandProject.java
@@ -1,15 +1,45 @@
 package main;
 
+import layers.Drums;
+import layers.Melody;
+
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 public class RandProject {
     protected Random rc = new Random();
-    private ArrayList<String> drums;
-    private Object melody;
+    private ArrayList<String> moods = new ArrayList(List.of(
+       "Pensive", "Happy", "Energetic", "Angry", "Depressing",
+            "Calm", "Anxious", "Content", "Frantic", "Nostalgic"
+    ));
+
+    private ArrayList<String> signatures = new ArrayList(List.of(
+            "4/4", "3/4", "5/7", "2/4", "6/8", "5/4"
+    ));
+
+    private Drums drums;
+    private Melody melody;
     private int tempo;
     private int maxTracks;
     private String mood;
     private String timeSignature;
+
+    public RandProject() {
+        this.drums = new Drums();
+        this.melody = new Melody();
+        this.tempo = rc.nextInt(80, 145);
+        this.maxTracks = rc.nextInt(3, 12);
+        this.mood = moods.get(rc.nextInt(0, moods.size()));
+        this.timeSignature = signatures.get(rc.nextInt(0, signatures.size()));
+    }
+
+    public StringBuilder getProjectDetails() {
+        var result = new StringBuilder();
+        result.append(String.format("//GENERAL DETAILS//\nTempo: %s\nMaximum tracks: %s\n", this.tempo, this.maxTracks));
+        result.append(String.format("Mood: %s\nTime signature: %s\n\n", this.mood, this.timeSignature));
+        result.append(String.format("//DRUMS//\n%s\n\n//MELODY//\n%s\n", this.drums.getResult(), this.melody.getResult()));
+        return result;
+    }
 
 }
